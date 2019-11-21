f4612d8e54b784b969eeae42223761550429d3c0
Aapo Halminen <ah.turha@gmail.com>
2019-11-21 20:21:48 +0200

    Uusi yritys ruuvitagiin

--- custom_components/ruuvitag-sensor/get-data.py
+++ custom_components/ruuvitag-sensor/get-data.py
@@ -1,18 +1,14 @@
 from ruuvitag_sensor.ruuvi import RuuviTagSensor
-from ruuvitag_sensor.data_formats import DataFormats
-from ruuvitag_sensor.decoder import get_decoder
 
 def handle_data(found_data):
-    # Tuple, jossa dataformaatti ja itse data
-    #(data_format, encoded) = DataFormats.convert_data(found_data)
-    #sensor_data = get_decoder(data_format).decode_data(encoded)
-
-    # Kirjoitetaan tiedostoon
-    #file = open("/usr/share/hassio/homeassistant/custom_components/ruuvitag-sensor/tag1.json", "w")
-    #file.write(sensor_data)
-    #file.close()
-    print(found_data[1])
+    # Kirjoitetaan tiedostoon json-muodossa data
+    file = open("/usr/share/hassio/homeassistant/custom_components/ruuvitag-sensor/tag1.json", "w")
+    file.write(found_data[1]) # Oikeat tiedot ovat toisena taulukossa, ensimmäisenä on mac-osoite
+    file.close()
+   
 
 # Vain saunan mittari: 
 mac = 'D7:FE:5A:B0:AB:59'
+
+# Tämä pyörittää etsintää ad infinitum
 RuuviTagSensor.get_datas(handle_data, mac)
