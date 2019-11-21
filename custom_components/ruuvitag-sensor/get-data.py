e580f4aa8a1a1b8d7d031295a1dc4cddcc4fc1c5
Aapo Halminen <ah.turha@gmail.com>
2019-11-21 19:58:59 +0200

    Lisätty ruuvitag-sensorille skripti, joka tallentaa sensorin tietoja tiedostoon.

---
+++ custom_components/ruuvitag-sensor/get-data.py
@@ -0,0 +1,17 @@
+from ruuvitag_sensor.ruuvi import RuuviTagSensor
+from ruuvitag_sensor.data_formats import DataFormats
+from ruuvitag_sensor.decoder import get_decoder
+
+def handle_data(found_data):
+    # Tuple, jossa dataformaatti ja itse data
+    (data_format, encoded) = DataFormats.convert_data(found_data)
+    sensor_data = get_decoder(data_format).decode_data(encoded)
+
+    # Kirjoitetaan tiedostoon
+    file = open("/usr/share/hassio/homeassistant/custom_components/ruuvitag-sensor/tag1.json", "w")
+    file.write(sensor_data)
+    file.close()
+
+# Vain saunan mittari: 
+macs = ['D7:FE:5A:B0:AB:59']
+RuuviTagSensor.get_datas(handle_data, macs)
