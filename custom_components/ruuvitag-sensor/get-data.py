b5440408fe41da5225858e3766478df9afe9469a
Aapo Halminen <ah.turha@gmail.com>
2019-11-21 20:19:29 +0200

    testi

--- custom_components/ruuvitag-sensor/get-data.py
+++ custom_components/ruuvitag-sensor/get-data.py
@@ -4,14 +4,15 @@ from ruuvitag_sensor.decoder import get_decoder
 
 def handle_data(found_data):
     # Tuple, jossa dataformaatti ja itse data
-    (data_format, encoded) = DataFormats.convert_data(found_data)
-    sensor_data = get_decoder(data_format).decode_data(encoded)
+    #(data_format, encoded) = DataFormats.convert_data(found_data)
+    #sensor_data = get_decoder(data_format).decode_data(encoded)
 
     # Kirjoitetaan tiedostoon
-    file = open("/usr/share/hassio/homeassistant/custom_components/ruuvitag-sensor/tag1.json", "w")
-    file.write(sensor_data)
-    file.close()
+    #file = open("/usr/share/hassio/homeassistant/custom_components/ruuvitag-sensor/tag1.json", "w")
+    #file.write(sensor_data)
+    #file.close()
+    print(found_data[1])
 
 # Vain saunan mittari: 
-macs = ['D7:FE:5A:B0:AB:59']
-RuuviTagSensor.get_datas(handle_data, macs)
+mac = 'D7:FE:5A:B0:AB:59'
+RuuviTagSensor.get_datas(handle_data, mac)
